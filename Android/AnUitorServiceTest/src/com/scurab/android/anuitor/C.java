05e73bc83b91bf0e14a07f53ad737122220bd9f7
Jiri Bruchanov <jbruchanov@gmail.com>
2014-05-15 11:37:23 +0200

    Init commit

---
+++ Android/AnUitorServiceTest/src/com/scurab/android/anuitor/C.java
@@ -0,0 +1,11 @@
+package com.scurab.android.anuitor;
+
+/**
+ * Created by jbruchanov@gmail.com
+ *
+ * @since 2014-05-15.
+ */
+public final class C {
+
+    public static final String MANIFEST = "../SampleApp/AndroidManifest.xml";
+}
---
+++ Android/SampleApp/src/com/scurab/android/anuitor/sample/SampleApplication.java
@@ -0,0 +1,16 @@
+package com.scurab.android.anuitor.sample;
+
+import android.app.Application;
+
+/**
+ * Created by jbruchanov@gmail.com
+ *
+ * @since 2014-05-15.
+ */
+public class SampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
