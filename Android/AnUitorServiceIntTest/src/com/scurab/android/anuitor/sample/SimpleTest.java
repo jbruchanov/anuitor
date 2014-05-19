0d929f6c7f31423a7261411f1727c5f7a7eb7d2c
Jiri Bruchanov <jbruchanov@gmail.com>
2014-05-19 15:02:17 +0100

    Update android project files

---
+++ Android/AnUitorServiceIntTest/src/com/scurab/android/anuitor/sample/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.scurab.android.anuitor.sample;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by jbruchanov on 19/05/2014.
+ */
+public class SimpleTest extends AndroidTestCase {
+
+    public void testSimple() {
+        assertNotNull(getContext());
+    }
+}
