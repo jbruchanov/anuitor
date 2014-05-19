958c17b9b5b8436550e2c92b413987b93e829295
Jiri Bruchanov <jbruchanov@gmail.com>
2014-05-19 16:06:34 +0100

    Add mockito to instrumentation test

--- Android/AnUitorServiceIntTest/src/com/scurab/android/anuitor/sample/SimpleTest.java
+++ Android/AnUitorServiceIntTest/src/com/scurab/android/anuitor/sample/SimpleTest.java
@@ -1,13 +1,28 @@
 package com.scurab.android.anuitor.sample;
 
+import android.content.Context;
 import android.test.AndroidTestCase;
+
+import com.scurab.android.anuitor.TestHelper;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by jbruchanov on 19/05/2014.
  */
 public class SimpleTest extends AndroidTestCase {
 
+    @Override
+    protected void setUp() throws Exception {
+        TestHelper.initMockito(getContext());
+        super.setUp();
+    }
+
     public void testSimple() {
         assertNotNull(getContext());
+    }
+
+    public void testMockSimple() {
+        Context c = mock(Context.class);
+        assertNotNull(c);
     }
 }
