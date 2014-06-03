aa09b5b3c945563b40506ec983666a01fd475b7e
Jiri Bruchanov <jbruchanov@gmail.com>
2014-06-03 16:47:00 +0100

    Add simple page selection content

--- GWT/AnUitor/src/com/scurab/gwt/anuitor/client/AnUitor.java
+++ GWT/AnUitor/src/com/scurab/gwt/anuitor/client/AnUitor.java
@@ -1,16 +1,20 @@
 package com.scurab.gwt.anuitor.client;
 
-import java.util.logging.Filter;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-
 import thothbot.parallax.core.client.RenderingPanel;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.scurab.gwt.anuitor.client.ui.TestPage;
+import com.scurab.gwt.anuitor.client.ui.ThreeDScene;
+import com.scurab.gwt.anuitor.client.ui.TreeView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -20,16 +24,56 @@ public class AnUitor implements EntryPoint {
      * This is the entry point method.
      */
     @Override
-    public void onModuleLoad() {        
-//        TestPage tp = new TestPage();
-//        RootPanel.get().add(tp);
-        RenderingPanel renderingPanel = new RenderingPanel();
-//        renderingPanel.setWidth("200px");
-//        renderingPanel.setHeight("200px");
-        // Background color
-        renderingPanel.setBackground(0x111111);
-        renderingPanel.setAnimatedScene(new MyScene());
+    public void onModuleLoad() {
+        History.addValueChangeHandler(new ValueChangeHandler<String>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<String> event) {
+                if (event.getValue().length() == 0) {
+                    addSelectionPane();
+                }
+            }
+        });
+        addSelectionPane();
+    }
 
-        RootLayoutPanel.get().add(renderingPanel);
+    private void addSelectionPane() {
+        HorizontalPanel hp = new HorizontalPanel();
+        Button screen = new Button("ScreenPreview");
+        screen.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                openWidget(((Button) event.getSource()).getText(), new TestPage());
+            }
+        });
+        hp.add(screen);
+        Button triD = new Button("3D");
+        triD.setWidth("120px");
+        triD.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                RenderingPanel renderingPanel = new RenderingPanel();
+                renderingPanel.setBackground(0x111111);
+                ThreeDScene scene = new ThreeDScene();
+                renderingPanel.setAnimatedScene(scene);
+                openWidget(((Button) event.getSource()).getText(), renderingPanel);
+            }
+        });
+        hp.add(triD);
+        Button vh = new Button("ViewHierarchy");
+        vh.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                openWidget(((Button) event.getSource()).getText(), new TreeView());
+            }
+        });
+        hp.add(vh);
+        RootLayoutPanel.get().clear();
+        RootLayoutPanel.get().add(hp);
+    }
+
+    private void openWidget(String v, IsWidget w) {
+        History.newItem(v);
+        RootLayoutPanel.get().clear();
+        RootLayoutPanel.get().add(w);
     }
 }
