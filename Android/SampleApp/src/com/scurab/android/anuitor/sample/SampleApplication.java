e4bf64eafd1a949df46fc1e4706422b107f627b0
Jiri Bruchanov <jbruchanov@gmail.com>
2014-06-03 16:51:12 +0100

    Remove obsolete content

--- Android/SampleApp/src/com/scurab/android/anuitor/sample/SampleApplication.java
+++ Android/SampleApp/src/com/scurab/android/anuitor/sample/SampleApplication.java
@@ -1,9 +1,7 @@
 package com.scurab.android.anuitor.sample;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
-import com.scurab.android.anuitor.nanoplugin.KnowsActivity;
 import com.scurab.android.anuitor.service.AnUitorService;
 
 /**
@@ -11,9 +9,7 @@ import com.scurab.android.anuitor.service.AnUitorService;
  *
  * @since 2014-05-15.
  */
-public class SampleApplication extends Application implements KnowsActivity {
-
-    private Activity mActivity;
+public class SampleApplication extends Application {
 
     @Override
     public void onCreate() {
@@ -21,14 +17,5 @@ public class SampleApplication extends Application implements KnowsActivity {
         Intent i = new Intent(this, AnUitorService.class);
         i.setAction(AnUitorService.START);
         startService(i);
-    }
-
-    public void setCurrentActivity(Activity activity) {
-        mActivity = activity;
-    }
-
-    @Override
-    public Activity getCurrentActivity() {
-        return mActivity;
     }
 }
