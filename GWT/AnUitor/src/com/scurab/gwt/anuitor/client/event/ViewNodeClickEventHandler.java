604852d0381541b8ad909939670053052995ed76
Jiri Bruchanov <jbruchanov@gmail.com>
2014-06-04 14:48:25 +0100

    Add click event

---
+++ GWT/AnUitor/src/com/scurab/gwt/anuitor/client/event/ViewNodeClickEventHandler.java
@@ -0,0 +1,7 @@
+package com.scurab.gwt.anuitor.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface ViewNodeClickEventHandler extends EventHandler {        
+    void onViewNodeClick(ViewNodeClickEvent event);
+}
