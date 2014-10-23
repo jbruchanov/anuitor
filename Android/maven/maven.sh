ac403fc436120605edfa8b56998db97c51e30157
Jiri Bruchanov <jbruchanov@gmail.com>
2014-10-24 00:09:26 +0100

    Release v0.9.3

--- Android/maven/maven.sh
+++ Android/maven/maven.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gpg -ab anuitor-0.9.1.pom
-gpg -ab anuitor-0.9.1.jar
-gpg -ab anuitor-0.9.1-sources.jar   
-gpg -ab anuitor-0.9.1-javadoc.jar 
-jar -cvf anuitor-0.9.1-bundle.jar anuitor-0.9.1.pom anuitor-0.9.1.pom.asc anuitor-0.9.1.jar anuitor-0.9.1.jar.asc anuitor-0.9.1-javadoc.jar anuitor-0.9.1-javadoc.jar.asc anuitor-0.9.1-sources.jar anuitor-0.9.1-sources.jar.asc
+gpg -ab anuitor-0.9.3.pom
+gpg -ab anuitor-0.9.3.jar
+gpg -ab anuitor-0.9.3-sources.jar   
+gpg -ab anuitor-0.9.3-javadoc.jar 
+jar -cvf anuitor-0.9.3-bundle.jar anuitor-0.9.3.pom anuitor-0.9.3.pom.asc anuitor-0.9.3.jar anuitor-0.9.3.jar.asc anuitor-0.9.3-javadoc.jar anuitor-0.9.3-javadoc.jar.asc anuitor-0.9.3-sources.jar anuitor-0.9.3-sources.jar.asc
