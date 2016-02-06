01126981bf7154b507490f8578a9d5059152f574
Jiri Bruchanov <jbruchanov@gmail.com>
2016-02-06 17:58:16 +0000

    Add DataResponse, update ResourceResponse

--- Android/service/src/main/java/com/scurab/android/anuitor/model/ResourceResponse.java
+++ Android/service/src/main/java/com/scurab/android/anuitor/model/ResourceResponse.java
@@ -6,22 +6,10 @@ import com.scurab.android.anuitor.hierarchy.IdsHelper;
 /**
  * Created by jbruchanov on 09/06/2014.
  */
-public class ResourceResponse {
+public class ResourceResponse extends DataResponse {
     @SerializedName("Type")
     public IdsHelper.RefType type;
 
     @SerializedName("Id")
     public int id;
-
-    @SerializedName("Name")
-    public String name;
-
-    @SerializedName("Data")
-    public Object data;
-
-    @SerializedName("Context")
-    public Object context;
-
-    @SerializedName("DataType")
-    public String dataType;
 }
