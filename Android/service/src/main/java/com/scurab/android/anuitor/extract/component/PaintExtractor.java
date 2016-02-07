440d3a03b69f137578cc75bc2174240caed3213a
Jiri Bruchanov <jbruchanov@gmail.com>
2016-02-07 12:35:57 +0000

    Update Paint extractor

--- Android/service/src/main/java/com/scurab/android/anuitor/extract/component/PaintExtractor.java
+++ Android/service/src/main/java/com/scurab/android/anuitor/extract/component/PaintExtractor.java
@@ -20,50 +20,50 @@ public class PaintExtractor extends BaseExtractor<Paint> {
     @Override
     public HashMap<String, Object> fillValues(Paint paint, HashMap<String, Object> data, HashMap<String, Object> contextData) {
         if (paint != null && data != null) {
-            data.put("PaintColor", getStringColor(paint.getColor()));
-            data.put("PaintAscent", paint.ascent());
-            data.put("PaintDescent", paint.descent());
-            data.put("PaintAlpha", paint.getAlpha());
-            data.put("PaintFlags", getBinaryString(paint.getFlags()));
-            data.put("PaintFontMetricsInt", paint.getFontMetricsInt().toString());
-            data.put("PaintFontSpacing", paint.getFontSpacing());
-            appendClassName("PaintMaskFilter", paint.getMaskFilter(), data);
-            appendClassName("PaintPathEffect", paint.getPathEffect(), data);
-            appendClassName("PaintRasterizer", paint.getRasterizer(), data);
-            appendClassName("PaintShader", paint.getShader(), data);
-            data.put("PaintStrokeCap", paint.getStrokeCap().name());
-            data.put("PaintStrokeJoin", paint.getStrokeJoin().name());
-            data.put("PaintStrokeMiter", paint.getStrokeMiter());
-            data.put("PaintStrokeWidth", paint.getStrokeWidth());
-            data.put("PaintStyle", paint.getStyle().name());
-            data.put("PaintTextAlign", paint.getTextAlign().name());
-            data.put("PaintTextScaleX", paint.getTextScaleX());
-            data.put("PaintTextSize", paint.getTextSize());
-            data.put("PaintTextSkewX", paint.getTextSkewX());
-            data.put("PaintTypeface", paint.getTypeface());
-            appendClassName("PaintXFermode", paint.getXfermode(), data);
-            data.put("PaintIsAntiAlias", paint.isAntiAlias());
-            data.put("PaintIsDither", paint.isDither());
-            data.put("PaintIsFakeBoldText", paint.isFakeBoldText());
-            data.put("PaintIsFilterBitmap", paint.isFilterBitmap());
-            data.put("PaintIsLinearText", paint.isLinearText());
-            data.put("PaintIsStrikeThruText", paint.isStrikeThruText());
-            data.put("PaintIsSubpixelText", paint.isSubpixelText());
-            data.put("PaintIsUnderlineText", paint.isUnderlineText());
-            data.put("PaintIsUnderlineText", paint.isUnderlineText());
+            data.put("Color", getStringColor(paint.getColor()));
+            data.put("Ascent", paint.ascent());
+            data.put("Descent", paint.descent());
+            data.put("Alpha", paint.getAlpha());
+            data.put("Flags", getBinaryString(paint.getFlags()));
+            data.put("FontMetricsInt", paint.getFontMetricsInt().toString());
+            data.put("FontSpacing", paint.getFontSpacing());
+            appendClassName("MaskFilter", paint.getMaskFilter(), data);
+            appendClassName("PathEffect", paint.getPathEffect(), data);
+            appendClassName("Rasterizer", paint.getRasterizer(), data);
+            appendClassName("Shader", paint.getShader(), data);
+            data.put("StrokeCap", paint.getStrokeCap().name());
+            data.put("StrokeJoin", paint.getStrokeJoin().name());
+            data.put("StrokeMiter", paint.getStrokeMiter());
+            data.put("StrokeWidth", paint.getStrokeWidth());
+            data.put("Style", paint.getStyle().name());
+            data.put("TextAlign", paint.getTextAlign().name());
+            data.put("TextScaleX", paint.getTextScaleX());
+            data.put("TextSize", paint.getTextSize());
+            data.put("TextSkewX", paint.getTextSkewX());
+            data.put("Typeface", paint.getTypeface());
+            appendClassName("XFermode", paint.getXfermode(), data);
+            data.put("IsAntiAlias", paint.isAntiAlias());
+            data.put("IsDither", paint.isDither());
+            data.put("IsFakeBoldText", paint.isFakeBoldText());
+            data.put("IsFilterBitmap", paint.isFilterBitmap());
+            data.put("IsLinearText", paint.isLinearText());
+            data.put("IsStrikeThruText", paint.isStrikeThruText());
+            data.put("IsSubpixelText", paint.isSubpixelText());
+            data.put("IsUnderlineText", paint.isUnderlineText());
+            data.put("IsUnderlineText", paint.isUnderlineText());
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-                data.put("PaintHinting", paint.getHinting());
+                data.put("Hinting", paint.getHinting());
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                data.put("PaintTextLocale", paint.getTextLocale().toString());
+                data.put("TextLocale", paint.getTextLocale().toString());
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                data.put("PaintFontFeatureSettings", paint.getFontFeatureSettings());
-                data.put("PaintLetterSpacing", paint.getLetterSpacing());
-                data.put("PaintIsElegantTextHeight", paint.isElegantTextHeight());
+                data.put("FontFeatureSettings", paint.getFontFeatureSettings());
+                data.put("LetterSpacing", paint.getLetterSpacing());
+                data.put("IsElegantTextHeight", paint.isElegantTextHeight());
             }
         }
         return data;
