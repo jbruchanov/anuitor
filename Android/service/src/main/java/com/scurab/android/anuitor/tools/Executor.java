fe3504ef63c09bccf30ebdcd4bd99dea03d95668
Jiri Bruchanov <jbruchanov@gmail.com>
2016-02-19 10:14:56 +0000

    Update executor

--- Android/service/src/main/java/com/scurab/android/anuitor/tools/Executor.java
+++ Android/service/src/main/java/com/scurab/android/anuitor/tools/Executor.java
@@ -1,6 +1,5 @@
 package com.scurab.android.anuitor.tools;
 
-import android.graphics.Bitmap;
 import android.os.Handler;
 import android.os.Looper;
 
@@ -9,26 +8,38 @@ import android.os.Looper;
  */
 public class Executor {
 
+    private static Handler sHandler;
+
     /**
      * Run op in main thread with 2s timeout
+     *
      * @param op
      */
     public static void runInMainThreadBlocking(final Runnable op) {
-        runInMainThreadBlocking(new Handler(Looper.getMainLooper()), op, 2000);
+        runInMainThreadBlocking(getHandler(), op, 2000);
     }
 
     /**
      * Run op in main thread with 2s timeout only if it's crashing...
      * First try is started in current thread
+     *
      * @param op
      */
     public static void runInMainThreadBlockingOnlyIfCrashing(final Runnable op) {
         try {
             op.run();
         } catch (Throwable t) {
-            runInMainThreadBlocking(new Handler(Looper.getMainLooper()), op, 2000);
+            runInMainThreadBlocking(getHandler(), op, 2000);
         }
     }
+
+    private static Handler getHandler() {
+        if (sHandler == null) {
+            sHandler = new Handler(Looper.getMainLooper());
+        }
+        return sHandler;
+    }
+
     /**
      * Run code in main thread and block current running thread
      * @param handler
@@ -37,7 +48,6 @@ public class Executor {
      */
     public static void runInMainThreadBlocking(Handler handler, final Runnable op, int timeout) {
         final Object lock = new Object();
-        final Bitmap[] output = new Bitmap[1];
         synchronized (lock) {
             handler.post(new Runnable() {
                 @Override
