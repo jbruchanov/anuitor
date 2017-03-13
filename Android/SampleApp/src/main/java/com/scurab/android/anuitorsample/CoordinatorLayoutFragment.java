cd178090626263f382c8ef6381bbe3927d3bc92e
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-13 14:47:05 +0000

    Clean

--- Android/SampleApp/src/main/java/com/scurab/android/anuitorsample/CoordinatorLayoutFragment.java
+++ Android/SampleApp/src/main/java/com/scurab/android/anuitorsample/CoordinatorLayoutFragment.java
@@ -8,7 +8,6 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.NestedScrollingParent;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
