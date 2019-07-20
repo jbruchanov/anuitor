1a0b90a541b05d9dbe9038722580ba9a5316d955
Jiri Bruchanov <jbruchanov@gmail.com>
2019-07-20 16:44:34 +0100

    Update code to use Map iface

--- Android/service/src/main/java/com/scurab/android/anuitor/reflect/ActivityThreadReflector.java
+++ Android/service/src/main/java/com/scurab/android/anuitor/reflect/ActivityThreadReflector.java
@@ -2,10 +2,10 @@ package com.scurab.android.anuitor.reflect;
 
 import android.app.Activity;
 import android.app.Application;
-import android.util.ArrayMap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by JBruchanov on 20/01/2017.
@@ -33,7 +33,7 @@ public class ActivityThreadReflector extends Reflector<Object> {
 
     public List<Activity> getActivities() {
         final ArrayList<Activity> result = new ArrayList<>();
-        for (Object mActivityRecord : ((ArrayMap<?, Object>) getFieldValue(mReal, "mActivities")).values()) {
+        for (Object mActivityRecord : ((Map<?, Object>) getFieldValue(mReal, "mActivities")).values()) {
             result.add(getFieldValue(mActivityRecord, "activity"));
         }
         return result;
